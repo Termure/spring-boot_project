a1afd2e6a635595a9650e21a632b673455d90603
Ioan Termure <ioan.termure24@gmail.com>
2023-05-22 17:37:43 +0300

    Unit tests exceptions when tutorials already exists

--- src/test/java/com/workspace/myapplication/service/TutorialServiceTests.java
+++ src/test/java/com/workspace/myapplication/service/TutorialServiceTests.java
@@ -1,6 +1,9 @@
 package com.workspace.myapplication.service;
 
 import static org.mockito.ArgumentMatchers.any;
+
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,10 +11,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.workspace.myapplication.exception.ResourceNotFoundException;
 import com.workspace.myapplication.model.Tutorial;
 import com.workspace.myapplication.repository.TutorialRepository;
 import com.workspace.myapplication.service.implementation.TutorialServiceImpl;
@@ -49,5 +56,23 @@ public class TutorialServiceTests {
 
         // then - verify the output
         Assertions.assertThat(savedTutorial).isNotNull();
+    }
+
+    @DisplayName("JUnit test for createTutorial which throw a custom exception")
+    @Test 
+    public void givenTutorialObject_whenCreateTutorial_thenThrowException(){
+        // given - preconditions or setup
+        List<Tutorial> tutorials = new ArrayList<Tutorial>();
+        tutorials.add(tutorial);
+        given(tutorialRepository.findByTitleContaining(tutorial.getTitle()))
+                .willReturn(tutorials);
+
+        // when - action or the behavior that we are going to test
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            tutorialService.createTutorial(tutorial);
+        });
+
+        // then - verify the output
+        verify(tutorialRepository, never()).save(tutorial);
     }
 }
