453b93843fea71ec5e9633c3cbbf746369da0786
Ioan Termure <ioan.termure24@gmail.com>
2023-07-27 16:42:27 +0300

    Adds Integration test from create tutorial rest api

---
+++ src/test/java/com/workspace/myapplication/integration/TutorialControllerTests.java
@@ -0,0 +1,69 @@
+package com.workspace.myapplication.integration;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.workspace.myapplication.model.Tutorial;
+import com.workspace.myapplication.repository.TutorialRepository;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test") 
+public class TutorialControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TutorialRepository tutorialRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup(){
+        tutorialRepository.deleteAll();
+    }
+
+    @DisplayName("Junit Integration test for save tutorial")
+    @Test
+    void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws Exception{
+
+        // given - precondition or setup
+        Tutorial tutorial = Tutorial.builder()
+                .title("some titile")
+                .description("some description")
+                .published(true)
+                .build();
+    
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/tutorials")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(tutorial)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title",
+                        is(tutorial.getTitle())))
+                .andExpect(jsonPath("$.description",
+                        is(tutorial.getDescription())))
+                .andExpect(jsonPath("$.published",
+                        is(tutorial.isPublished())));
+    }
+}
